a722d840b25d55fdfbaa02ee5f7b622867754de7
TERMANATOR2121 <70167096+TERMANATOR2121@users.noreply.github.com>
2021-10-03 13:53:37 -0700

    Add files via upload

---
+++ Particle.js
@@ -0,0 +1,26 @@
+class Particle {
+    constructor(x, y, r) {
+
+        var options = {
+            restitution: 0.4
+        }
+        this.r = r;
+
+        this.color = color(random(0, 255), random(0, 255), random(0, 255));
+        this.body = Bodies.circle(x, y, this.r, options);
+        World.add(world, this.body);
+    }
+
+    display() {
+
+        var pos = this.body.position;
+        push();
+        translate(pos.x, pos.y);
+        noStroke();
+        fill(this.color);
+        ellipseMode(RADIUS);
+        ellipse(0, 0, this.r, this.r);
+        pop();
+    }
+
+};
